814bfdc0ba31885c95540160f0fc8f2ff62b2b5f
ravindra281298 <ravindra281298@gmail.com>
2020-10-08 22:46:48 +0530

    added update and delete operation

--- routes/src.php
+++ routes/src.php
@@ -3,13 +3,23 @@
     require_once '../db/users.php';
 
     session_start();
+    $index_url = '../views/index.php';
+    $addUser_url = '../views/addUser.php';
+
+    // $_SESSION['first_name'] = '';
+    // $_SESSION['last_name'] = '';
+    // $_SESSION['gender'] = '';
+    // $_SESSION['country'] = '';
+    // $_SESSION['dob'] = '';
+    // $_SESSION['id'] = 0;
+    // $_SESSION['update'] = false;
 
     if(isset($_POST['save'])) {
 
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
         $gender = $_POST['gender'];
-        $address = $_POST['country'];
+        $country = $_POST['country'];
         $dob = $_POST['dob'];
 
         $mysqli->query("INSERT INTO users (first_name, last_name, gender, country, dob)
@@ -19,7 +29,57 @@
         $_SESSION['message'] = 'Record has been saved';
         $_SESSION['msg_type'] = "success";
 
-        $url = '../views/index.php';
-        header("location: $url ");
+        header("location: $index_url ");
+    }
+
+    if(isset($_GET['delete'])) {
+
+        $id = $_GET['delete'];
+
+        $mysqli->query("DELETE FROM users where id = $id") or die($mysqli->error);
+
+        $_SESSION['message'] = 'Record has been deleted';
+        $_SESSION['msg_type'] = 'danger';
+
+        header("Refresh:2; location: $index_url ");
+    }
+
+    if(isset($_GET['edit'])) {
+        $id = $_GET['edit'];
+
+        $result = $mysqli->query("SELECT * FROM users where id = $id") or die($mysqli->error);
+
+        // if($row = $result->fetch_assoc()) {
+            $row = $result->fetch_assoc();
+            $_SESSION['first_name'] = $row['first_name'];
+            $_SESSION['last_name'] = $row['last_name'];
+            $_SESSION['gender'] = $row['gender'];
+            $_SESSION['country'] = $row['country'];
+            $_SESSION['dob'] = $row['dob'];
+            $_SESSION['update'] = true;
+            $_SESSION['id'] = $id;
+
+        // }
+
+        header("location: $addUser_url");
+    }
+
+    if(isset($_POST['update'])) {
+
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $gender = $_POST['gender'];
+        $country = $_POST['country'];
+        $dob = $_POST['dob'];
+        $id = $_POST['id'];
+
+        $mysqli->query("UPDATE users SET first_name='$first_name', last_name='$last_name', 
+                        gender='$gender', country='$country', dob='$dob' WHERE id='$id' ")
+                        or die($mysqli->error);
+        
+        $_SESSION['message'] = 'Record has been update.';
+        $_SESSION['msg_type'] = 'warning';
+
+        header("location: $index_url ");
     }
 ?>
--- views/addUser.php
+++ views/addUser.php
@@ -10,24 +10,31 @@
         crossorigin="anonymous">
     </head>
     <body>
+        <?php require_once '../routes/src.php' ?>
         <div class="container">
+
         <h1>Add Person</h1><hr>
         <form class="form-group" action="../routes/src.php" method="post">
+            <input type="hidden" name="id", value = <?php if(isset($_SESSION['id'])) { echo $_SESSION['id']; unset($_SESSION['id']); } ?>>
             <label>First Name</label>
-            <input class="form-control" type="text" name="first_name" placeholder="Enter your First Name">
+            <input class="form-control" type="text" name="first_name" value = "<?php if(isset($_SESSION['first_name'])) { echo $_SESSION['first_name']; unset($_SESSION['first_name']); } ?>" placeholder="Enter your First Name">
             <label>Last Name</label>
-            <input class="form-control" type="text" name="last_name" placeholder="Enter your Last Name">
+            <input class="form-control" type="text" name="last_name" value = "<?php if(isset($_SESSION['last_name'])) { echo $_SESSION['last_name']; unset($_SESSION['last_name']); } ?>" placeholder="Enter your Last Name">
             <label>Gender</label>
-            <select class="form-control" name="gender" placeholder="Select your Gender">
+            <select class="form-control" name="gender" value = "<?php if(isset($_SESSION['gender'])) { echo $_SESSION['gender']; unset($_SESSION['gender']); } ?>" placeholder="Select your Gender">
             <option name="male">Male</option>
             <option name="female">Female</option>
             <option name="other">Other</option>
             </select>
             <label>Country</label>
-            <input class="form-control" type="text" name="country" placeholder="Enter your Country">
+            <input class="form-control" type="text" name="country" value = "<?php if(isset($_SESSION['country'])) { echo $_SESSION['country']; unset($_SESSION['country']); } ?>" placeholder="Enter your Country">
             <label>Date of Birth</label>
-            <input class="form-control" type="date" name="dob"><br>
-            <button class="btn btn-info" type="submit" name="save">Save</button>
+            <input class="form-control" type="date" name="dob" value = "<?php if(isset($_SESSION['dob'])) { echo $_SESSION['dob']; unset($_SESSION['dob']); } ?>"><br>
+            <?php if(isset($_SESSION['update'])): unset($_SESSION['update']); ?>
+                <button class="btn btn-info" type="submit" name="update">Update</button>
+            <?php else: ?>
+                <button class="btn btn-info" type="submit" name="save">Save</button>
+            <?php endif ?>
         </form>
         </div>
     </body>
